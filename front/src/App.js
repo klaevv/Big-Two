97df078836a61a6456333a317cd7868b8c3e9c51
virtalas <virtalas@users.noreply.github.com>
2020-12-02 17:30:02 +0200

    Use webrtc to exchange card plays

--- front/src/App.js
+++ front/src/App.js
@@ -1,19 +1,100 @@
-import React, { useEffect } from 'react'
-import { Route, useLocation } from 'react-router-dom'
+/* eslint-disable */
+
+import React, { useEffect, useState } from 'react'
+import { useLocation } from 'react-router-dom'
+import { LioWebRTC } from "react-liowebrtc"
 
 import HomePage from './pages/HomePage/HomePage'
 
+function contains(obj, list) {
+  let i
+  for (i = 0; i < list.length; i++) {
+    if (list[i] === obj) {
+      return true
+    }
+  }
+  return false
+}
+
 function App() {
   const location = useLocation()
+
+  const [peers, setPeers] = useState([]) // Players
+  // TODO: bug: chatLog resets? Try with 3 players.
+  const [chatLog, setChatLog] = useState([]) // Right now just 'Player joined' notifications
+  const [hand, setHand] = useState(['s1', 's2', 's3']) // The cards the player is holding.
+  const [table, setTable] = useState([]) // Most recent play (combination of cards).
+  const [wrtc, setWrtc] = useState() // TODO: Temp solution
 
   useEffect(() => {
     // eslint-disable-next-line no-console
     console.log('pathname', location.pathname)
   }, [location])
 
+  const join = (webrtc) => {
+    webrtc.joinRoom('react-liowebrtc-test-demo')
+    setWrtc(webrtc)
+  }
+
+  const addChat = (name, message, alert = false) => {
+    setChatLog(chatLog.concat({
+      name,
+      message: `${message}`,
+      timestamp: `${Date.now()}`,
+      alert
+    }))
+  }
+
+  const handleCreatedPeer = (webrtc, peer) => {
+    addChat(`Peer-${peer.id.substring(0, 5)} joined the room!`, ' ', true)
+    setPeers([...peers, peer])
+  }
+
+    // const handleShut = () => {
+  //   this.setState({ containerClass: 'container animated hinge' })
+  //   this.props.webrtc.leaveRoom()
+  //   this.props.webrtc.disconnect()
+  // }
+
+  const handlePeerData = (webrtc, type, payload, peer) => {
+    console.log('received', type, payload)
+    switch (type) {
+      case 'play':
+        setTable(payload)
+        break
+      default:
+        return
+    };
+  }
+
+  const handleRemovedPeer = (webrtc, peer) => {
+    setPeers(peers.filter(p => !p.closed))
+  }
+
+  const sendPlay = (cards) => {
+    if (wrtc) {
+      console.log(cards)
+      wrtc.shout('play', cards)
+    }
+    setTable(cards)
+    setHand(hand.filter(c => !contains(c, cards)))
+  }
+
   return (
     <div className="App">
-      <Route exact path="/" component={HomePage} />
+      <LioWebRTC
+        onReady={join}
+        onCreatedPeer={handleCreatedPeer}
+        onRemovedPeer={handleRemovedPeer}
+        onReceivedPeerData={handlePeerData}
+      >
+        <HomePage
+          chatLog={chatLog}
+          hand={hand}
+          table={table}
+          sendPlay={sendPlay}
+        />
+      </LioWebRTC>
     </div>
   )
 }
--- front/src/pages/HomePage/HomePage.js
+++ front/src/pages/HomePage/HomePage.js
@@ -1,15 +1,48 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
-import './HomePage.scss'
+const HomePage = (props) => {
+  const { chatLog, sendPlay, table, hand } = props
 
-const HomePage = () => {
+  const generateChats = () => {
+    return chatLog.map((item) => (
+      <div className="chat" key={`chat-${item.name}-${item.timestamp}`}>
+        <b className="name" style={{ color: '#888' }}>{item.name}</b>
+      </div>
+    ))
+  }
+
+  const handlePlayCard = () => {
+    if (hand.length > 0) { // Check that combination is legit (possible to play)
+      sendPlay([hand[0]]) // Send a combination or a single card.
+    }
+  }
+
   return (
     <div className="homePage">
       <div className="homePageContainer">
         <h1>Big Two</h1>
+        {generateChats()}
+        <span style={{ color: '#888' }}>On the table: </span>
+        {table.map(card => <span style={{ color: '#888' }} key={card}>{card}</span>)}
+        <br />
+        <button type="button" disabled={hand.length === 0} onClick={handlePlayCard}>Play a card</button>
       </div>
     </div>
   )
+}
+
+HomePage.propTypes = {
+  chatLog: PropTypes.arrayOf(PropTypes.object),
+  sendPlay: PropTypes.func,
+  table: PropTypes.arrayOf(PropTypes.string),
+  hand: PropTypes.arrayOf(PropTypes.string),
+}
+HomePage.defaultProps = {
+  chatLog: [],
+  sendPlay: () => {},
+  table: [],
+  hand: [],
 }
 
 export default HomePage
